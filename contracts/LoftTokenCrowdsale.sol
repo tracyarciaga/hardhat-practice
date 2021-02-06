6243bd00206d668f1f4ee5f098738bd7ae2f80f4
Teresa Li <tracy_arciaga@hotmail.com>
2021-02-06 19:37:40 +0800

    Added changeRate func.

--- contracts/LoftTokenCrowdsale.sol
+++ contracts/LoftTokenCrowdsale.sol
@@ -12,7 +12,8 @@ contract LoftTokenCrowdsale is Crowdsale, AllowanceCrowdsale, TimedCrowdsale, Po
 
   Roles.Role private _admin;
   uint8 private _stage;
-  uint256 private _secondSaleOpening;
+  // uint256 private _secondSaleOpening;
+  uint256 private _stageRate;
 
   mapping(address => uint256) public contributions;
   enum CrowdsaleStage { First, Second }
@@ -38,14 +39,14 @@ contract LoftTokenCrowdsale is Crowdsale, AllowanceCrowdsale, TimedCrowdsale, Po
 
     // Set deployer as admin
     _admin.add(msg.sender);
-
-    // Set state
+    _stageRate = _rate;
+    // Set stage
     // TODO: Comment if doing two deployments per stage.
     // _stage = uint8(CrowdsaleStage.First);
     // _secondSaleOpening = _closingTime + 30 days;
 
     if (_crowdsaleStage == uint8(CrowdsaleStage.Second)) {
-      require(_rate >= 200);
+      require(_rate >= 150 && _rate <= 200, "Invalid rate for the main sale.");
     }
 
   }
@@ -68,7 +69,6 @@ contract LoftTokenCrowdsale is Crowdsale, AllowanceCrowdsale, TimedCrowdsale, Po
   }
 
  
-
   /**
   * TODO: Uncomment this function if doing single deployment only.
   * @dev Allows admin to start the second sale.
@@ -87,14 +87,39 @@ contract LoftTokenCrowdsale is Crowdsale, AllowanceCrowdsale, TimedCrowdsale, Po
   * @param closingTime new closingTime
   */
  function extendTime(uint256 closingTime) public {
-    require(_admin.has(msg.sender), "Calling address does not have admin role");
+    require(super.isOpen(), "Sale is already closed.")
+    require(_admin.has(msg.sender), "Calling address does not have admin role.");
     if (_stage == uint8(CrowdsaleStage.First)) {
       // Presale Closing time (can be adjusted to a maximum of 60 days)
-      require(closingTime > (now + 60 days), "Cannot extend first sale to more than 60 days");
+      require(closingTime > (now + 60 days), "Cannot extend first sale to more than 60 days.");
     }
     super._extendTime(closingTime);
   }
 
+
+  /**
+  * @dev Allows admin to change rate of the second sale.
+  * @param closingTime new closingTime
+  */
+  function changeRate(uint256 rate) public {
+    require(super.isOpen(), "Sale is already closed.")
+    require(_admin.has(msg.sender), "Calling address does not have admin role.");
+    require(_stage == uint8(CrowdsaleStage.Second), "Invalid stage. Cannot change rate for other stages.");
+    _stageRate = rate
+    uint256 weiAmount = msg.value;
+    super._getTokenAmount(weiAmount);
+  }
+
+  /**
+  * @dev Overrides parent method taking into account variable rate.
+  * @param weiAmount The value in wei to be converted into tokens
+  * @return The number of tokens _weiAmount wei will buy at present time
+  */
+  function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
+    uint256 currentRate = _stageRate;
+    return _stageRate.mul(weiAmount);
+  }
+}
 
   /**
   * @dev Extend parent behavior by updating contributions.
