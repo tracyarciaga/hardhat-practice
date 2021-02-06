7673cd7fc08c9be54b5cbfdc5062f6ab49e4923d
Teresa Li <tracy_arciaga@hotmail.com>
2021-02-06 11:59:13 +0800

    Revert override buyTokens

--- contracts/LoftTokenCrowdsale.sol
+++ contracts/LoftTokenCrowdsale.sol
@@ -32,14 +32,19 @@ contract LoftTokenCrowdsale is Crowdsale, TimedCrowdsale {
     return contributions[_beneficiary];
   }
 
-
-  function buyTokens(
-    address beneficiary
+  /**
+  * @dev Override for extensions that require an internal state to check for validity (current user contributions,
+  * etc.)
+  * @param beneficiary Address receiving the tokens
+  * @param weiAmount Value in wei involved in the purchase
+  */
+  function _updatePurchasingState(
+    address beneficiary,
+    uint256 weiAmount
   )
-    public nonReentrant payable
+    internal
   {
-    uint256 weiAmount = msg.value;
-    super.buyTokens(beneficiary);
+    super._updatePurchasingState(beneficiary, weiAmount);
     uint256 _existingContribution = contributions[beneficiary];
     uint256 _newContribution = _existingContribution.add(weiAmount);
     contributions[beneficiary] = _newContribution;
